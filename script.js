4289c9b44174b1c41757b7dd2177e1d034b34303
Garen <garendemirdogen1@gmail.com>
2022-07-24 22:52:46 -0400

    added api pull, button for movies

--- script.js
+++ script.js
@@ -22,11 +22,79 @@ var getBook = function (num) {
 // get movie function
 
 $(document).ready(function () {
-  function movieFinder() {
-    const apiKey = "db0633cb7d87ac12e2e4f675d677f8a5";
-    var title = $("input").val();
-    var movieAPI = `https://api.themoviedb.org/3/search/movie?query=${title}&api_key=${apiKey}`;
+  var movies = [];
+
+  $(document).on("click", ".movie-btn", getData);
+
+  function getData() {
+    var movie = $(this).attr("data-name");
+    var queryURL =
+      "https://www.omdbapi.com/?t=" + movie + "&type=movie&apikey=e7412d0b";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+
+      if (response.Response === "True") {
+        var movieData = $("#output");
+        var movieCard = $("<div>");
+        movieCard.attr("class", "card");
+
+        //Get data
+        var title = $("<h3>").text('"' + response.Title + '"');
+        var rating = $("<p>").text("Rated: " + response.Rated);
+        var summary = $("<p>").text("Plot: " + response.Plot);
+        var poster = $("<img>");
+        poster.attr("src", response.Poster);
+
+        //dump to divs
+        movieCard.append(title, rating, summary, poster);
+        movieData.prepend(movieCard);
+      }
+      // Modal to alert user that is not a valid movie title
+      // alert for now until I figure out the modals
+      else {
+        alert("ERROR: No results found for " + movie);
+        var errorMovieIndex = movies.indexOf(movie);
+        if (errorMovieIndex > -1) {
+          movies.splice(errorMovieIndex, 1);
+        }
+        makeButtons();
+      }
+    });
   }
+
+  function makeButtons() {
+    $("#btn-div").empty();
+    for (var i = 0; i < movies.length; i++) {
+      //make button, append to movies
+      var a = $("<button>");
+      //add class
+      a.addClass("movie-btn");
+      a.addClass("btn btn-primary");
+      //add name
+      a.attr("data-name", movies[i]);
+      //add text
+      a.text(movies[i]);
+      //add button to div
+      $("#btn-div").append(a);
+    }
+  }
+
+  $("#submit").on("click", function () {
+    //grab user input
+    var newMovie = $("#input").val().trim();
+    movies.push(newMovie);
+    console.log(movies);
+    makeButtons();
+
+    //clear input
+    $("#input").val("");
+  });
+
+  makeButtons();
 });
 /* Example Book: 
     The Alchemist
